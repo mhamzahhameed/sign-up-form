e442a9f51aa5cbe4941d9522ff01a1a0e85c1117
Mhamzahhameed <hamzahraja11@gmail.com>
2022-10-24 14:26:52 +0500

    email authentication

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useState } from "react";
+import "./App.css";
+import app from "./Firebase";
+
+function App() {
+  const auth = getAuth(app);
+
+  const [email, setEmail] = useState();
+
+  const [password, setPassword] = useState();
+
+  const signUp = () => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        alert("Successfully Created An Account");
+        // ...
+      })
+
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode);
+        // ..
+      });
+  };
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        alert("This User Has Successfully Signed In");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        alert(errorCode);
+      });
+  };
+
+  return (
+    <div className='main'>
+      <div className='App'>
+        <input
+          type='email'
+          placeholder='please add valid email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='please enter password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={signUp}>Create Account</button>
+        <button onClick={signIn}> Sign in</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWXPFms7mMTxDREuAVYkArD9_0E4pRz_o",
+  authDomain: "email-auth-8d6ee.firebaseapp.com",
+  projectId: "email-auth-8d6ee",
+  storageBucket: "email-auth-8d6ee.appspot.com",
+  messagingSenderId: "858325142867",
+  appId: "1:858325142867:web:460c8b0cf2a676d85ea5a6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
